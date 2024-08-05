5be9cfa265af22bdcbd80ed8535a8f487aa54aba
Audrey <ahda.audrey92@gmail.com>
2024-08-05 16:06:06 +0200

    Ajout nom et photo dans user defaults et finalsiation bouton annuler

--- Tip-stop/Model/Utilisateur.swift
+++ Tip-stop/Model/Utilisateur.swift
@@ -5,11 +5,12 @@
 //  Created by Apprenant 122 on 18/07/2024.
 //
 import Foundation
+import UIKit
 
 struct Utilisateur: Identifiable {
     let id = UUID()
     var nom: String
-    var photo: String
+    var photo: UIImage?
     var favoris: [Favori]
 
 }
