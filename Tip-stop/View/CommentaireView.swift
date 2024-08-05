0bf4f954d81d82149f428bf1c9186b312c20b997
“diakhite-sio” <diakhite.ibrahima.sio@gmail.com>
2024-08-05 15:09:27 +0200

    Ajout des topics avec une list et le commencement des reponses prévu pour chaque topics

---
+++ Tip-stop/View/CommentaireView.swift
@@ -0,0 +1,18 @@
+//
+//  CommentaireView.swift
+//  Tip-stop
+//
+//  Created by Apprenant 163 on 05/08/2024.
+//
+
+import SwiftUI
+
+struct CommentaireView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CommentaireView()
+}
---
+++ Tip-stop/View/NewCommentaire.swift
@@ -0,0 +1,18 @@
+//
+//  NewCommentaire.swift
+//  Tip-stop
+//
+//  Created by Apprenant 163 on 05/08/2024.
+//
+
+import SwiftUI
+
+struct NewCommentaire: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewCommentaire()
+}
