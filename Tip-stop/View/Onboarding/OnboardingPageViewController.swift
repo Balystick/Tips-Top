57b94a7949a6d8414b08f7fc19a3c9a6d02e83d4
Balystick <balystick74@icloud.com>
2024-08-06 10:44:39 +0200

    Add viewWillAppear & viewWillDisappear functions to sync video playback with user navigation

--- Tip-stop/View/Onboarding/OnboardingPageViewController.swift
+++ Tip-stop/View/Onboarding/OnboardingPageViewController.swift
@@ -11,10 +11,10 @@ import AVFoundation
 
 /// Extension de UIView pour la lecture en boucle des vidéos
 extension UIView {
-    func playVideoInLoop(fileName: String, fileType: String) {
+    func playVideoInLoop(fileName: String, fileType: String) -> AVPlayer? {
         guard let path = Bundle.main.path(forResource: fileName, ofType: fileType) else {
             debugPrint("\(fileName).\(fileType) not found")
-            return
+            return nil
         }
         
         let player = AVPlayer(url: URL(fileURLWithPath: path))
@@ -29,6 +29,7 @@ extension UIView {
         }
         
         player.play()
+        return player
     }
 }
 
@@ -42,14 +43,24 @@ class OnboardingPageViewController: UIViewController {
     var videoName: String?
     var titleText: String?
     var descriptionText: String?
+    var player: AVPlayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if let videoName = videoName {
-            videoContainerView.playVideoInLoop(fileName: videoName, fileType: "mp4")
-        }
         titleLabel.text = titleText
         label.text = descriptionText
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let videoName = videoName {
+            player = videoContainerView.playVideoInLoop(fileName: videoName, fileType: "mp4")
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        player?.pause()
+        player?.seek(to: .zero)
     }
 }
