442d9060f7f8daf836fffba412f2d2f8da65dc1a
Balystick <balystick74@icloud.com>
2024-08-09 00:11:36 +0200

    Add hasSeenOnBoarding Key remover

--- Tip-stop/Tip_stopApp.swift
+++ Tip-stop/Tip_stopApp.swift
@@ -10,7 +10,8 @@ import SwiftUI
 @main
 struct Tip_stopApp: App {
     init() {
-        
+//          Supprime la clé hasSeenOnboarding du UserDefault afin de relancer l'onboarding pour les tests
+            UserDefaults.standard.removeObject(forKey: "hasSeenOnboarding")
     }
     var body: some Scene {
         WindowGroup {
