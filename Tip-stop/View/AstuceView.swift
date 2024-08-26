9d347d071413a1372f3dbc6a760ef4a980861ae6
Balystick <balystick74@icloud.com>
2024-08-26 17:46:46 +0200

    Fix static position for action buttons on tips Fix like management implementation Improve gesture handling for action buttons Correct reference to current tip comments

--- Tip-stop/View/AstuceView.swift
+++ Tip-stop/View/AstuceView.swift
@@ -2,134 +2,65 @@
 //  AstuceView.swift
 //  Tip-stop
 //
-//  Created by Apprenant 122 on 29/07/2024.
-//
+
 import SwiftUI
 import AVKit
 
+/// `AstuceView` représente une vue individuelle pour une astuce
 struct AstuceView: View {
     let astuce: Astuce
-    @State private var showingSteps = false
-    @State private var showingComments = false
     @State private var player: AVPlayer?
-    @State private var playerItem: AVPlayerItem?
-    @EnvironmentObject var viewModel: InfiniteScrollViewModel
-    @State private var isLiked: Bool = false
-    @State private var isFavorited: Bool = false
-    @State private var mutableAstuce: Astuce
-    @Binding var hasSeenOnboarding: Bool
+    // Index courant du TabView pour savoir si la vidéo doit être lue
     @Binding var currentIndex: Int
+    // Index de l'astuce dans la liste
     var index: Int
+    // Dictionnaire stockant les lecteurs vidéo pour chaque astuce
     @Binding var players: [Int: AVPlayer]
-    
-    init(astuce: Astuce, currentIndex: Binding<Int>, index: Int, players: Binding<[Int: AVPlayer]>, hasSeenOnboarding: Binding<Bool>) {
+
+    init(astuce: Astuce, currentIndex: Binding<Int>, index: Int, players: Binding<[Int: AVPlayer]>) {
         self.astuce = astuce
-        _mutableAstuce = State(initialValue: astuce)
         self._currentIndex = currentIndex
         self.index = index
         self._players = players
-        self._hasSeenOnboarding = hasSeenOnboarding
     }
-    
+
     var body: some View {
         GeometryReader { geometry in
             ZStack {
-                    if let player = player {
-                        VideoPlayer(player: player)
-                            .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-                            .onAppear {
-                                if currentIndex == index && hasSeenOnboarding {
-                                    player.play()
-                                }
-                                setupLooping()
+                if let player = player {
+                    VideoPlayer(player: player)
+                        .aspectRatio(contentMode: .fill)
+                        .onAppear {
+                            if currentIndex == index {
+                                player.play()
                             }
-                            .onDisappear {
-                                player.pause()
-                            }
-                        
-                    } else {
-                        Text("Loading video...")
-                            .onAppear {
-                                loadVideo()
-                            }
-                            .frame(maxWidth: .infinity, maxHeight: .infinity)
-                            .background(Color.black)
-                            .foregroundColor(.white)
-                            .font(.title)
-                    }
-                
-                VStack {
-                    Spacer()
-                    HStack {
-                        Spacer()
-                        VStack {
-                            Button(action: {
-                                toggleLike()
-                            }) {
-                                VStack {
-                                    Image(systemName: isLiked ? "heart.fill" : "heart")
-                                        .font(.title)
-                                        .foregroundColor(.white)
-                                    Text("\(UserDefaults.standard.integer(forKey: "likeCount_\(astuce.video)"))")
-                                        .foregroundColor(.white)
-                                        .font(.caption)
-                                }
-                                .padding()
-                            }
-                            
-                            Button(action: {
-                                showingComments = true
-                            }) {
-                                Image(systemName: "message")
-                                    .font(.title)
-                                    .foregroundColor(.white)
-                            }
-                            
-                            Button(action: {
-                                isFavorited.toggle()
-                                viewModel.toggleFavorite(for: mutableAstuce)
-                            }) {
-                                Image(systemName: isFavorited ? "star.fill" : "star")
-                                    .font(.title)
-                                    .foregroundColor(.white)
-                                    .padding()
-                            }
-
-                            Button(action: {
-                                showingSteps.toggle()
-                            }) {
-                                Image(systemName: "list.bullet.rectangle")
-                                    .font(.title)
-                                    .foregroundColor(.white)
-                                    .padding(.bottom, 40)
-                                    .padding(.top, 3)
-                            }
+                            setupLooping()
                         }
-                    }
+                        .onDisappear {
+                            player.pause()
+                        }
+                } else {
+                    Text("Loading video...")
+                        .onAppear {
+                            loadVideo()
+                        }
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .background(Color.black)
+                        .foregroundColor(.white)
+                        .font(.title)
                 }
-                .padding()
-                .frame(width: geometry.size.width, height: geometry.size.height)
             }
             .frame(width: geometry.size.width, height: geometry.size.height)
             .clipped()
-            .sheet(isPresented: $showingComments) {
-                CommentaireView(commentaires: astuce.commentaires)
-            }
-            .sheet(isPresented: $showingSteps) {
-                StepsView()
-            }
-            .onAppear {
-                isLiked = viewModel.getStoredLikeStatus(for: mutableAstuce.video)
-                isFavorited = viewModel.getStoredFavorite(for: mutableAstuce.video)
-            }
         }
         .edgesIgnoringSafeArea(.all)
     }
-    
+
+    /// Charge la vidéo associée à l'astuce
     private func loadVideo() {
-        let videoName = mutableAstuce.video
+        let videoName = astuce.video
         if let videoURL = Bundle.main.url(forResource: videoName, withExtension: "mp4") {
-            playerItem = AVPlayerItem(url: videoURL)
+            let playerItem = AVPlayerItem(url: videoURL)
             player = AVPlayer(playerItem: playerItem)
             player?.volume = 1.0
             players[index] = player
@@ -138,7 +69,8 @@ struct AstuceView: View {
             print("Failed to find video: \(videoName).mp4")
         }
     }
-    
+
+    /// Configure la lecture en boucle de la vidéo
     private func setupLooping() {
         player?.actionAtItemEnd = .none
         NotificationCenter.default.addObserver(
@@ -148,17 +80,6 @@ struct AstuceView: View {
         ) { [weak player] _ in
             player?.seek(to: .zero)
             player?.play()
-        }
-    }
-    
-    private func toggleLike() {
-        isLiked.toggle()
-        viewModel.toggleLike(for: mutableAstuce)
-        
-        if isLiked {
-            mutableAstuce.nombreDeLikes += 1
-        } else {
-            mutableAstuce.nombreDeLikes -= 1
         }
     }
 }
