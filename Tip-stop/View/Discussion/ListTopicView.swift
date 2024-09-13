007c12d40133909008aa3c0862780a5f31f1e9b1
Balystick <balystick74@icloud.com>
2024-09-13 16:30:13 +0200

    Merge Ibrahima

--- Tip-stop/View/Discussion/ListTopicView.swift
+++ Tip-stop/View/Discussion/ListTopicView.swift
@@ -8,49 +8,54 @@
 import SwiftUI
 import UIKit
 
-struct ListTopicView: View {
-    
-    // Ajout navigation path & globalDataModel
+struct ListTopicView: View{
     @Binding var path: NavigationPath
+    @StateObject private var viewModel = TopicViewModel()
     
-    var allTopic:[Topic] = [
-        Topic(id: UUID(), dateDebut: Date(), sujet: "Les androïdes rêvent-ils de moutons électriques ?", reponse: [rep1], categorie: categorie1),
-        Topic(id: UUID(), dateDebut: Date(), sujet: "Les dauphins volent-ils dans l'eau", reponse: [rep1], categorie: categorie1),
-        Topic(id: UUID(), dateDebut: Date(), sujet: "Une carrote dans la terre est-elle une carrote qui ce noit ?", reponse: [rep1], categorie: categorie1)]
-        
-        var body: some View
+    
+    /*
+     [Topic(dateDebut: Date(), sujet: "Les androïdes rêvent-ils de moutons électriques ?", reponse: [rep1], categorie: categorie1),
+     Topic(dateDebut: Date(), sujet: "Les dauphins volent-ils dans l'eau", reponse: [rep1], categorie: categorie1),
+     Topic(dateDebut: Date(), sujet: "Une carrote dans la terre est-elle une carrote qui ce noit ?", reponse: [rep1], categorie: categorie1)]
+     */
+     
+    var body: some View
+    {
+        VStack
         {
             NavigationView
             {
-                List(allTopic)
+                List(viewModel.topics)
                 {
-                   
-                    topic in NavigationLink(destination: TopicView(sujetName: topic.sujet, categorieName: topic.categorie.titre))
+                    topic in NavigationLink(destination: TopicView(viewModel: viewModel,topic: topic))
                     {
                         Text(topic.sujet)
+                        
+                        
                     }
+                    .navigationTitle("List of topics")
+                }.onAppear{viewModel.fetchTopics()}
+                
+                
+                
+                
+                    NavigationLink(destination: AddTopic(viewModel: viewModel), label:{
+                        ZStack(alignment:.trailing)
+                        {
+                            Circle()
+                                .frame(width:50)
+                        }.padding(.leading,200)
+                        
+                        
+                    })
                 }
-    
-            }
-            .navigationBarBackButtonHidden(true)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button(action: {
-                        path.removeLast()
-                    }) {
-                        Image(systemName: "arrow.uturn.left")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 35, height: 35)
-                            .foregroundColor(Color(white: 0.2))
-                    }
-                }
-            }
-           
+            
         }
+        
+        
+    }
     
 }
-
 
 
 
@@ -65,3 +70,77 @@ struct ListTopicView: View {
     ListTopicView(path: .constant(NavigationPath()))
 }
 
+
+
+
+
+
+
+
+////
+////  ListTopicView.swift
+////  Tip-stop
+////
+////  Created by Apprenant 163 on 04/08/2024.
+////
+//
+//import SwiftUI
+//import UIKit
+//
+//struct ListTopicView: View {
+//    
+//    // Ajout navigation path & globalDataModel
+//    @Binding var path: NavigationPath
+//    
+//    var allTopic:[Topic] = [
+//        Topic(id: UUID(), dateDebut: Date(), sujet: "Les androïdes rêvent-ils de moutons électriques ?", reponse: [rep1], categorie: categorie1),
+//        Topic(id: UUID(), dateDebut: Date(), sujet: "Les dauphins volent-ils dans l'eau", reponse: [rep1], categorie: categorie1),
+//        Topic(id: UUID(), dateDebut: Date(), sujet: "Une carrote dans la terre est-elle une carrote qui ce noit ?", reponse: [rep1], categorie: categorie1)]
+//        
+//        var body: some View
+//        {
+//            NavigationView
+//            {
+//                List(allTopic)
+//                {
+//                   
+//                    topic in NavigationLink(destination: TopicView(sujetName: topic.sujet, categorieName: topic.categorie.titre))
+//                    {
+//                        Text(topic.sujet)
+//                    }
+//                }
+//    
+//            }
+//            .navigationBarBackButtonHidden(true)
+//            .toolbar {
+//                ToolbarItem(placement: .navigationBarLeading) {
+//                    Button(action: {
+//                        path.removeLast()
+//                    }) {
+//                        Image(systemName: "arrow.uturn.left")
+//                            .resizable()
+//                            .scaledToFit()
+//                            .frame(width: 35, height: 35)
+//                            .foregroundColor(Color(white: 0.2))
+//                    }
+//                }
+//            }
+//           
+//        }
+//    
+//}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//#Preview {
+//    ListTopicView(path: .constant(NavigationPath()))
+//}
+//
--- Tip-stop/View/Discussion/NewReponse.swift
+++ Tip-stop/View/Discussion/NewReponse.swift
@@ -8,29 +8,82 @@
 import SwiftUI
 import UIKit
 
-struct NewReponse: UIViewControllerRepresentable {
+struct NewReponse: View {
     
-    @State var textCommentaire:String = ""
-    var date = Date()
-    var user1 = utilisateur1
+    @StateObject var viewModel:TopicViewModel
+    @State var reponseText:String = ""
     
-        func makeUIViewController(context: Context) -> NewReponseController
+    var body: some View
+    {
+        VStack(alignment: .leading)
         {
-            guard let finalVC = UIStoryboard(name: "Main", bundle: .main).instantiateViewController(withIdentifier: "NewReponseController")as? NewReponseController
-            else
+           HStack
             {
-                fatalError("Unable to Instantiate Detail View Controller")
+                TextField("new reponse",text: $reponseText)
+                    .frame(width: 200,height: 100)
+                
+                Button(action: {
+//                    viewModel.reponses.append(Reponse(date: Date(), contenu: reponseText, utilisateur: utilisateur1))
+                }, label:
+                        {
+                    ZStack
+                     {
+                         RoundedRectangle(cornerRadius: 10)
+                             .frame(width: 75,height: 50)
+                         Text("Send")
+                             .foregroundStyle(.white)
+                     }
+                })
             }
-            return finalVC
-        }
-        
-    
-        func updateUIViewController(_ viewController: NewReponseController,context: Context)
-        {
             
         }
+    }
+    
+     
 }
 
-#Preview {
+/*#Preview {
     NewReponse()
-}
+}*/
+
+
+
+
+
+
+////
+////  NewCommentaire.swift
+////  Tip-stop
+////
+////  Created by Apprenant 163 on 05/08/2024.
+////
+//
+//import SwiftUI
+//import UIKit
+//
+//struct NewReponse: UIViewControllerRepresentable {
+//    
+//    @State var textCommentaire:String = ""
+//    var date = Date()
+//    var user1 = utilisateur1
+//    
+//        func makeUIViewController(context: Context) -> NewReponseController
+//        {
+//            guard let finalVC = UIStoryboard(name: "Main", bundle: .main).instantiateViewController(withIdentifier: "NewReponseController")as? NewReponseController
+//            else
+//            {
+//                fatalError("Unable to Instantiate Detail View Controller")
+//            }
+//            return finalVC
+//        }
+//        
+//    
+//        func updateUIViewController(_ viewController: NewReponseController,context: Context)
+//        {
+//            
+//        }
+//}
+//
+//#Preview {
+//    NewReponse()
+//}
